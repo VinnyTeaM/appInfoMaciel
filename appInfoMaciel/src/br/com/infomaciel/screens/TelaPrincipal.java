a772302bc463f7fde75c79a277e67bb09acbade3
avinn <avinn@Vinny-w11>
2023-05-23 01:19:59 -0300

    removendo alguns import que não foram usados

--- appInfoMaciel/src/br/com/infomaciel/screens/TelaPrincipal.java
+++ appInfoMaciel/src/br/com/infomaciel/screens/TelaPrincipal.java
@@ -2,23 +2,17 @@ package br.com.infomaciel.screens;
 
 import java.awt.Color;
 import java.awt.Dialog.ModalExclusionType;
-import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.IOException;
 import java.sql.Connection;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.imageio.ImageIO;
 import javax.swing.DebugGraphics;
 import javax.swing.ImageIcon;
 import javax.swing.JDesktopPane;
